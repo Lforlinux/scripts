8e99885444d050c014100f5536db8ecc64475acf
KLN <karthik.kln@gmail.com>
2017-05-03 12:35:16 +0530

    now refer kln-shell for all bash scripts

--- findcommand.sh
+++
@@ -1,2 +0,0 @@
-find a/b/c -typed -exec chmod 755 {} \;
-find a/b/c -typef -exec chmod 755 {} \;
--- history.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bashi
-/bin/date "+%H-%M-%S_%d-%m-%Y" | tee /tmp/curtime.txt
-history -a /home/history/`cat /tmp/curtime.txt`_history.txt
