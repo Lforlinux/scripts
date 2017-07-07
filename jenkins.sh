aeb340828b98f47de4e84959bef9065ed4af0459
KLN <karthik.kln@gmail.com>
2017-07-07 15:48:59 +0530

    bash.lforlinux.com rsync deployment script

--- jenkins.sh
+++ jenkins.sh
@@ -1,2 +1,2 @@
 #!/bin/bash -e
-/usr/bin/rsync -rlDvcC --progress /var/lib/jenkins/workspace/bash.lforlinux.com/* /home/kln/GIT/scripts
+/usr/bin/rsync -rlDvcC --progress /var/lib/jenkins/workspace/bash.lforlinux.com/* /home/bash/scripts
