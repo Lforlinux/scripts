cf02f23447c2924ab059a3c9daa45b2c36b1658b
KLn on thinkpad <karthik.kln@gmail.com>
2017-08-18 22:04:39 +0530

    new traffic added

---
+++ python/trafficsingal.py
@@ -0,0 +1,11 @@
+signal = {}
+signal['red']='stop'
+signal['yellow']='about to red'
+signal['green']='go'
+signal = {'red' : 'stop', 'yellow' : 'about to red', 'green' : 'go'}
+
+/
+Things to do in this script
+ask for input as a prompt
+if green the output is go
+if red output is appropriate 
