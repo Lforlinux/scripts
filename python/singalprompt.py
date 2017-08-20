ef82dd725a29370abac3e0b13f87fb0646212978
KLn on thinkpad <karthik.kln@gmail.com>
2017-08-20 17:55:47 +0530

    promt traffic signal promt

---
+++ python/singalprompt.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# This is some secure program that uses security.
+import sys
+
+signal = 'red' #this is our password.
+
+input = raw_input('Please Enter colour: ')
+
+if input == singal:
+    print 'please stop!'
+elif:
+    print 'Gonow!'
+    sys.exit(0)
+
+print 'Welcome!'
